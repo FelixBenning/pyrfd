98a784dbffe58f5fcb9da18d5d1d551731489fd2
Felix Benning <felix.benning@gmail.com>
2024-02-21 10:02:58 +0100

    add quantile plots

--- pyrfd/covariance.py
+++ pyrfd/covariance.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod
-from cProfile import label
 from logging import warning
 import math
 import pandas as pd
@@ -7,11 +6,24 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import scipy.stats as stats
+from scipy import stats
+
+def selection(sorted_list, num_elts):
+    """
+    return a selection of num_elts from the sorted_list (evenly spaced in the index)
+    always includes the first and last index
+    """
+    if len(sorted_list) < num_elts:
+        return sorted_list
+    idxs = np.round(np.linspace(0, len(sorted_list)-1, num_elts)).astype(int)
+    return sorted_list[idxs]
 
 
-def fit_mean_var(batch_sizes, batch_losses, max_bootstrap=100):
-    b_inv = np.array([1 / b for b in batch_sizes])
+
+def fit_mean_var(batch_sizes:np.array, batch_losses:np.array, max_bootstrap=100):
+    batch_sizes = np.array(batch_sizes)
+    batch_losses = np.array(batch_losses)
+    b_inv = 1/batch_sizes
 
     # we initialize with the assumption that the variance at batch_size=Inf is zero
     var_reg = LinearRegression(fit_intercept=True)
@@ -23,8 +35,8 @@ def fit_mean_var(batch_sizes, batch_losses, max_bootstrap=100):
     for idx in range(max_bootstrap):
         vars = var_reg.predict(b_inv.reshape(-1, 1))  # variance at batchsizes 1/b in X
 
-        mu = np.average(batch_losses, weights=[1 / v for v in vars])
-        centered_squares = [(Lb - mu) ** 2 for Lb in batch_losses]
+        mu = np.average(batch_losses, weights=(1/vars))
+        centered_squares = (batch_losses - mu) ** 2
 
         old_intercept = var_reg.intercept_
         # fourth moments i.e. 3*sigma^4 = 3 * var^2 are the variance of the centered
@@ -33,7 +45,7 @@ def fit_mean_var(batch_sizes, batch_losses, max_bootstrap=100):
         var_reg.fit(
             b_inv.reshape(-1, 1),
             centered_squares,
-            sample_weight=[1 / v**2 for v in vars],
+            sample_weight=1/vars ** 2,
         )
 
         if math.isclose(old_intercept, var_reg.intercept_):
@@ -104,8 +116,9 @@ class CovarianceModel:
         ## ============================
         ### === sanity check plots ===
         ## ============================
+        grouped = df.groupby("batchsize", sort=True)
         b_size_grouped = (
-            df.groupby("batchsize", sort=True)
+            grouped
             .agg(
                 loss_mean=pd.NamedAgg(column="loss", aggfunc="mean"),
                 loss_var=pd.NamedAgg(
@@ -122,90 +135,120 @@ class CovarianceModel:
         reduced_df = df.groupby("batchsize").head(100).reset_index(drop=True)
         b_size_inv = 1 / reduced_df["batchsize"]
 
-        fig, axs = plt.subplots(3)
+        fig, axs = plt.subplots(3,2)
 
         ## ==== Plot Losses =====
-        axs[0].set_xscale("log")
-        axs[0].set_xlabel("1/b")
+        axs[0,0].set_xscale("log")
+        # axs[0,0].set_xlabel("1/b")
 
         # scatterplot
-        axs[0].scatter(
+        axs[0,0].scatter(
             b_size_inv,
             reduced_df["loss"],
             s=1,  # marker size
             label=r"$\mathcal{L}_b(w)$",
         )
         # batchwise means
-        axs[0].plot(
+        axs[0,0].plot(
             b_size_g_inv,
             b_size_grouped["loss_mean"],
             marker="*",
             label="batchwise mean",
         )
-        axs[0].plot(
+        axs[0,0].plot(
             b_size_g_inv,
             np.full_like(b_size_g_inv, fill_value=self.mean),
             label=rf"$\hat\mu={self.mean:.4}$",
         )
-        axs[0].fill_between(
+        axs[0,0].fill_between(
             x=b_size_g_inv,
             y1=self.mean + stats.norm.ppf(0.025) * np.sqrt(var_estimates),
             y2=self.mean + stats.norm.ppf(0.975) * np.sqrt(var_estimates),
             alpha=0.3,
         )
         # legend
-        axs[0].legend(loc="upper left")
+        axs[0,0].legend(loc="upper left")
+
+        ## QQplot
+        b_size_selection = np.array(selection(b_size_grouped["batchsize"], 5))[1:]
+        for bs in b_size_selection:
+            stats.probplot(grouped.get_group(bs)["loss"], dist="norm",plot=axs[0,1])
+        for idx, line in enumerate(axs[0,1].get_lines()):
+            line.set_color(f"C{idx//2}")
+            if idx % 2 == 0: # scatter
+                line.set_markersize(1)
+                line.set_label(f"b={b_size_selection[idx//2]}")
+            if idx % 2 == 1:
+                line.set_linestyle("--")
+
+        axs[0,1].set_xlabel("")
+        axs[0,1].legend()
+
 
         ## === Plot squared errors =====
-        axs[1].set_xlabel("1/b")
+        # axs[1,0].set_xlabel("1/b")
         # axs[1,0].set_xscale("log")
-        axs[1].scatter(
+        axs[1,0].scatter(
             b_size_inv,
             (reduced_df["loss"] - self.mean) ** 2,
             s=1,  # marker size
             label=r"$(\mathcal{L}_b(w)-\hat{\mu})^2$",
         )
-        axs[1].plot(
+        axs[1,0].plot(
             b_size_g_inv,
             b_size_grouped["loss_var"],
             marker="*",
             label="batchwise mean squares",
         )
-        axs[1].plot(
+        axs[1,0].plot(
             b_size_g_inv,
             var_estimates,
             label=r"Var$(\mathcal{L}_b(w))$",
         )
-        axs[1].fill_between(
+        axs[1,0].fill_between(
             x=b_size_g_inv,
             # χ^2 confidence bounds
             y1=var_estimates + (stats.chi2.ppf(0.025, df=1) - 1) * var_estimates,
             y2=var_estimates + (stats.chi2.ppf(0.975, df=1) - 1) * var_estimates,
             alpha=0.3,
         )
-        axs[1].legend(loc="upper left")
+        axs[1,0].legend(loc="upper left")
+
+        # QQ-plot against chi^2
+        for bs in b_size_selection:
+            stats.probplot((grouped.get_group(bs)["loss"] - self.mean) ** 2, dist=stats.chi2(df=1),plot=axs[1,1])
+        for idx, line in enumerate(axs[1,1].get_lines()):
+            line.set_color(f"C{idx//2}")
+            if idx % 2 == 0: # scatter
+                line.set_markersize(1)
+                line.set_label(f"b={b_size_selection[idx//2]}")
+            if idx % 2 == 1:
+                line.set_linestyle("--")
+        axs[1,1].set_title("")
+        axs[1,1].set_xlabel("")
+        axs[1,1].legend()
 
         # ==== Plot Gradient Norms ====
-        axs[2].set_xlabel("1/b")
-        axs[2].scatter(
+        axs[2,0].set_xlabel("1/b")
+        axs[2,0].scatter(
             b_size_inv,
             reduced_df["sq_grad_norm"],
             s=1,  # marker size
             label=r"$\|\nabla\mathcal{L}_b(w)\|^2$",
         )
-        axs[2].plot(
+        axs[2,0].plot(
             b_size_g_inv,
             b_size_grouped["sq_grad_norm_mean"],
             marker="*",
             label="batchwise mean",
         )
         sq_norm_means = self.g_var_reg.predict(b_size_g_inv.to_numpy().reshape(-1, 1))
-        axs[2].plot(
+        axs[2,0].plot(
             b_size_g_inv,
             sq_norm_means,
             label=r"$\mathbb{E}[\|\nabla\mathcal{L}_b(w)\|^2]$",
         )
-        axs[2].fill_between(
+        axs[2,0].fill_between(
             x=b_size_g_inv,
             y1=sq_norm_means
             + (stats.chi2.ppf(0.025, dims) - dims) * sq_norm_means / dims,
@@ -216,7 +259,19 @@ class CovarianceModel:
 
         # TODO: figure out wtf is wrong with the confidence interval of the squared norm
 
-        axs[2].legend(loc="upper left")
+        axs[2,0].legend(loc="upper left")
+
+        for bs in b_size_selection:
+            stats.probplot(grouped.get_group(bs)["sq_grad_norm"], dist=stats.chi2(df=dims),plot=axs[2,1])
+        for idx, line in enumerate(axs[2,1].get_lines()):
+            line.set_color(f"C{idx//2}")
+            if idx % 2 == 0: # scatter
+                line.set_markersize(1)
+                line.set_label(f"b={b_size_selection[idx//2]}")
+            if idx % 2 == 1:
+                line.set_linestyle("--")
+        axs[2,1].set_title("")
+        axs[2,1].legend()
 
         return {
             "mean": self.mean,
