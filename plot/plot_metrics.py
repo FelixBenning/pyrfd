92c6560c714dc29fd3c8a376a91ecb25f29cdf57
Felix Benning <felix.benning@gmail.com>
2024-05-16 16:09:32 +0200

    metrics and covariance fit

--- plot/plot_metrics.py
+++ plot/plot_metrics.py
@@ -1,18 +1,16 @@
-from math import e
-from telnetlib import DO
-import pandas as pd
-from pathlib import Path
 import re
+from pathlib import Path
 
-from matplotlib import legend, rc
+import pandas as pd
+
+from matplotlib import rc
 import matplotlib.pyplot as plt
-from sympy import plot
 
 
 rc("font", **{"family": "serif", "serif": ["Computer Modern"]})
 plt.rcParams.update(
     {
-        # 'text.usetex': True,
+        'text.usetex': True,
         "text.latex.preamble": r"\usepackage{amsfonts}"
     }
 )
@@ -71,19 +69,19 @@ def extract_metrics(problem_dir, plot_filter):
 
 # fmt: off
 PLOT_FILTER = {
-    "RFD(SqEx)": {
+    "RFD(SE)": {
         "includes": ["RFD", "SquaredExponential"],
         "excludes": ["b_size_inv"],
     },
-    "RFD(RatQ(beta=1))": {
+    "S-RFD(SE)": {
+        "includes": ["RFD", "SquaredExponential", "b_size_inv"],
+    },
+    "RFD(RQ(beta=1))": {
         "includes": ["RFD", "RationalQuadratic", "beta=1"],
     },
     "A-RFD": {
         "includes": ["SGD", "lr=14.2"],
     },
-    # "Adam(lr=1e-3)": {
-    #     "includes": ["Adam", "lr=0.001"],
-    # },
     "Adam(lr=1e-2)": {
         "includes": ["Adam", "lr=0.01"],
     },
@@ -138,7 +136,7 @@ def plot_final_loss_over_lr(ax, metrics, idx=None):
     reduced_df.set_index(["epoch", "learning_rate", "seed"], verify_integrity=True)
 
     epoch_df = reduced_df.groupby(["epoch"])
-    last_epoch_df = epoch_df.get_group(max(epoch_df.groups.keys(), key=int))
+    last_epoch_df = epoch_df.get_group((max(epoch_df.groups.keys(), key=int),))
     agg_df = last_epoch_df.groupby(["learning_rate"]).agg({"val/loss": [
         "mean",
         pd.NamedAgg("q0.1", lambda x: x.quantile(0.1)),
@@ -147,8 +145,8 @@ def plot_final_loss_over_lr(ax, metrics, idx=None):
 
     lines = ax.plot(agg_df.index, agg_df["mean"], label=metrics["name"], **plot_kwargs)
     ax.fill_between(agg_df.index, agg_df["q0.1"], agg_df["q0.9"], alpha=0.3, facecolor=lines[0].get_color())
-    ax.set_title("Step size tuning")
-    ax.set_xlabel("Learning Rate")
+    ax.set_title("Tuning")
+    ax.set_xlabel("Learning rate")
     ax.set_ylabel("Final validation loss")
     ax.set_xscale("log")
     ax.set_yscale("log")
@@ -165,7 +163,7 @@ def plot_train_loss(ax, metrics, **kwargs):
 def plot_validation_loss(ax, metrics, **kwargs):
     plot_metric(ax, metrics, "val/loss", epoch=True, **kwargs)
     ax.legend()
-    ax.set_title("Validation Loss")
+    ax.set_title("Validation loss")
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
     ax.set_yscale("log")
@@ -196,6 +194,7 @@ def plot_step_size(ax, metrics, **kwargs):
     plot_metric(ax, metrics, "step_size", epoch=False, **kwargs)
     ax.legend()
     ax.set_title("Step size")
+    ax.set_ylabel("Step size")
     ax.set_xlabel("Step")
     ax.set_yscale("log")
 
@@ -228,16 +227,28 @@ def spread_lr(df):
 
 def plot_summary(problem):
     problem_dir = Path(f"logs/{problem}")
-    wanted = ["RFD(SqEx)", "RFD(RatQ(beta=1))", "A-RFD"]
+    wanted = ["RFD(SE)", "S-RFD(SE)", "RFD(RQ(beta=1))", "A-RFD"]
     metrics = extract_metrics(problem_dir, plot_filter(wanted))
-    (fig, axs) = plt.subplots(1, 2, figsize=(10, 4))
+    (fig, axs) = plt.subplots(2, 2, figsize=(9, 7))
     for (idx, item) in enumerate(metrics):
-        plot_validation_loss(axs[0], item, idx=idx)
-        plot_initial_learning_rate(axs[1], item, idx=idx)
+        plot_validation_loss(axs[0,0], item, idx=idx)
+        plot_initial_learning_rate(axs[1,0], item, idx=idx)
+        plot_step_size(axs[1,1], item, idx=idx)
 
     metrics = extract_metrics(problem_dir, plot_filter(["Adam(lr=1e-2)", "SGD(lr=1)"]))
-    for (idx, item) in enumerate(metrics, start=3):
-        plot_validation_loss(axs[0], item, idx=idx)
+    for (idx, item) in enumerate(metrics, start=len(wanted)):
+        plot_validation_loss(axs[0,0], item, idx=idx)
+        plot_step_size(axs[1,1], item, idx=idx)
+
+    adam_metrics = extract_metrics(problem_dir, {"Adam": {"includes": ["Adam"]}})
+    adam_joined = pd.concat([spread_lr(x["metrics"]) for x in  adam_metrics])
+
+    sgd_metrics = extract_metrics(problem_dir, {"SGD": {"includes": ["SGD"]}})
+    sgd_joined = pd.concat([spread_lr(x["metrics"]) for x in  sgd_metrics])
+
+    plot_final_loss_over_lr(axs[0, 1], {"name": "Adam", "metrics": adam_joined}, idx=len(wanted))
+    plot_final_loss_over_lr(axs[0, 1], {"name": "SGD", "metrics": sgd_joined}, idx=len(wanted)+1)
+    axs[0,1].legend()
 
     fig.tight_layout()
     plt.savefig(f"plot/{problem}_summary.pdf")
@@ -245,21 +256,21 @@ def plot_summary(problem):
 def plot_performance(problem):
     problem_dir = Path(f"logs/{problem}")
     metrics = extract_metrics(problem_dir, PLOT_FILTER)
-    (fig, axs) = plt.subplots(2, 2, figsize=(10, 6))
+    (fig, axs) = plt.subplots(2, 2, figsize=(9, 6))
     for (idx, item) in enumerate(metrics):
         plot_validation_loss(axs[0, 0], item, idx=idx)
         plot_train_loss(axs[1, 0], item, idx=idx)
         plot_accuracy(axs[0, 1], item, idx=idx)
         axs[0,1].set_ylim(bottom=0.96, top=1)
-    
+
     adam_metrics = extract_metrics(problem_dir, {"Adam": {"includes": ["Adam"]}})
     adam_joined = pd.concat([spread_lr(x["metrics"]) for x in  adam_metrics])
 
     sgd_metrics = extract_metrics(problem_dir, {"SGD": {"includes": ["SGD"]}})
     sgd_joined = pd.concat([spread_lr(x["metrics"]) for x in  sgd_metrics])
 
-    plot_final_loss_over_lr(axs[1, 1], {"name": "Adam", "metrics": adam_joined}, idx=3)
-    plot_final_loss_over_lr(axs[1, 1], {"name": "SGD", "metrics": sgd_joined}, idx=4)
+    plot_final_loss_over_lr(axs[1, 1], {"name": "Adam", "metrics": adam_joined}, idx=4)
+    plot_final_loss_over_lr(axs[1, 1], {"name": "SGD", "metrics": sgd_joined}, idx=5)
     axs[1,1].legend()
 
     fig.tight_layout()
@@ -268,9 +279,9 @@ def plot_performance(problem):
 
 def plot_step_behavior(problem):
     problem_dir = Path(f"logs/{problem}")
-    wanted = ["RFD(SqEx)", "RFD(RatQ(beta=1))", "A-RFD"]
+    wanted = ["RFD(SE)", "RFD(RQ(beta=1))", "A-RFD"]
     metrics = extract_metrics(problem_dir, plot_filter(wanted))
-    (fig, axs) = plt.subplots(3, 2, figsize=(7, 10))
+    (fig, axs) = plt.subplots(3, 2, figsize=(9, 10))
     for (idx, item) in enumerate(metrics):
         plot_learning_rate(axs[0, 0], item, idx=idx)
         plot_initial_learning_rate(axs[1, 0], item, idx=idx)
@@ -281,7 +292,7 @@ def plot_step_behavior(problem):
         plot_param_norm(axs[2, 1], item, idx=idx)
 
     metrics = extract_metrics(problem_dir, plot_filter(["Adam(lr=1e-2)", "SGD(lr=1)"]))
-    for (idx, item) in enumerate(metrics, start=3):
+    for (idx, item) in enumerate(metrics, start=len(wanted)):
         plot_step_size(axs[2, 0], item, idx=idx)
         plot_dot_grad_param(axs[0, 1], item, idx=idx)
         plot_initial_dot_grad_param(axs[1, 1], item, idx=idx)
