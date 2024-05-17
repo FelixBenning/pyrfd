50d220b8d7a370f71e102a5db72fc330c0888729
Felix Benning <felix.benning@gmail.com>
2024-05-17 09:51:04 +0200

    logs and new plotting

--- plot/visualize_covariance_fit.py
+++ plot/visualize_covariance_fit.py
@@ -15,40 +15,70 @@ plt.rcParams.update({
     'text.latex.preamble': r'\usepackage{amsfonts}'
 })
 
-mnist = MNIST(batch_size=100)
-mnist.prepare_data()
-mnist.setup("fit")
+def visualize_fit():
+    mnist = MNIST(batch_size=100)
+    mnist.prepare_data()
+    mnist.setup("fit")
 
-cov_model = covariance.IsotropicCovariance()
+    cov_model = covariance.IsotropicCovariance()
 
-sampler = sampling.IsotropicSampler(
-    model_factory = CNN7,
-    loss= F.nll_loss,
-    data= mnist.data_train,
-    cache= "MNIST_CNN7_oversampled.csv"
-)
+    sampler = sampling.IsotropicSampler(
+        model_factory = CNN7,
+        loss= F.nll_loss,
+        data= mnist.data_train,
+        cache= "MNIST_CNN7_oversampled.csv"
+    )
 
-if len(sampler) == 0:
-    b_sizes = range(10, 100, 10)
-    sampler.sample(bsize_counts= pd.Series(
-        data=repeat(600, len(b_sizes)),
-        index= b_sizes
-    ))
-    b_sizes = range(100, 1001, 100)
-    sampler.sample(bsize_counts= pd.Series(
-        data=[60_000 // b_size for b_size in b_sizes],
-        index= b_sizes
-    ))
+    if len(sampler) == 0:
+        b_sizes = range(10, 100, 10)
+        sampler.sample(bsize_counts= pd.Series(
+            data=repeat(600, len(b_sizes)),
+            index= b_sizes
+        ))
+        b_sizes = range(100, 1001, 100)
+        sampler.sample(bsize_counts= pd.Series(
+            data=[60_000 // b_size for b_size in b_sizes],
+            index= b_sizes
+        ))
 
-cov_model.dims = sampler.dims
-cov_model.fit(sampler.snapshot_as_dataframe())
+    cov_model.dims = sampler.dims
+    cov_model.fit(sampler.snapshot_as_dataframe())
 
-print(f"asymptotic lr={cov_model.asymptotic_learning_rate()}")
+    print(f"asymptotic lr={cov_model.asymptotic_learning_rate()}")
 
-(fig, axs) = cov_model.plot_sanity_checks(
-    sampler.snapshot_as_dataframe(),
-    batch_sizes=[10,100,1000]
-)
-fig.tight_layout()
-plt.savefig("plot/MNIST_CNN7_covariance_fit.pdf")
-plt.show()
+    (fig, axs) = cov_model.plot_sanity_checks(
+        sampler.snapshot_as_dataframe(),
+        batch_sizes=[10,100,1000]
+    )
+    fig.tight_layout()
+    plt.savefig("plot/MNIST_CNN7_covariance_fit.pdf")
+
+def get_run_covariance(run):
+    mnist = MNIST(batch_size=100)
+    mnist.prepare_data()
+    mnist.setup("fit")
+
+    cov_model = covariance.IsotropicCovariance()
+    sampler = sampling.IsotropicSampler(
+        model_factory = CNN7,
+        loss= F.nll_loss,
+        data= mnist.data_train,
+        cache=f"cache/MNIST/CNN7_run={run}/covariance_cache.csv"
+    )
+    cov_model.dims = sampler.dims
+    cov_model.fit(sampler.snapshot_as_dataframe())
+    return cov_model
+
+def visualize_lr_variance():
+    asympt_lr = []
+    for run in range(20):
+        cov_model = get_run_covariance(run)
+        asympt_lr.append(cov_model.asymptotic_learning_rate())
+    
+    plt.hist(asympt_lr, label="Asymptotic learning rate")
+    plt.savefig("plot/MNIST_CNN7_asymptotic_lr.pdf")
+
+
+if __name__ == "__main__":
+    visualize_lr_variance()
+    visualize_fit()
