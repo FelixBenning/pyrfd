44300b3d37fffda2a211eefee886e08d2a0890f0
Felix Benning <felix.benning@gmail.com>
2025-02-03 11:21:19 +0100

    classification task serialization

--- classification/classification_task.py
+++ classification/classification_task.py
@@ -1,5 +1,7 @@
 """ ClassificationTask serialization """
+
 from pydoc import locate
+from sys import stdout
 from typing import Any
 from attr import dataclass
 import yaml
@@ -28,71 +30,77 @@ def full_name(cls):
     return f"{module}{cls.__qualname__}"
 
 
+def strip_exclamation(tag: str) -> str:
+    """Strip the exclamation mark from a YAML tag"""
+    assert tag.startswith("!"), f"Tag {tag} does not start with '!'"
+    return tag[1:]
+
+
 @dataclass
 class ClassificationTask:
     model_class: type[nn.Module]
     dataset_class: type[L.LightningDataModule]
-    params: Params[str, dict]
     loss_class: type[nn.Module] = nn.CrossEntropyLoss
-    seed: None | int = None
+    params: dict = {}
 
     @property
     def loss(self):
         """Return an instance of the loss function"""
-        return self.loss_class(self.params.loss)
+        return self.loss_class(self.params["loss"])
 
     @property
     def model(self):
         """Return an instance of the model"""
-        return self.model_class(self.params.model)
+        return self.model_class(self.params["model"])
 
     @property
     def dataset(self):
         """Return an instance of the dataset"""
-        return self.dataset_class(self.params.dataset)
+        return self.dataset_class(self.params["dataset"])
 
     @staticmethod
-    def to_yaml(dumper, data: "ClassificationTask"):
+    def to_yaml(dumper: yaml.Dumper, data: "ClassificationTask"):
         """Dump a ClassificationTask to a YAML file (registerd as representer)"""
-        return dumper.represent_mapping(
-            "!" + full_name(data.__class__),
-            {
-                "model": {
-                    "class": full_name(data.model_class),
-                    "params": data.params.model,
-                },
-                "dataset": {
-                    "class": full_name(data.dataset_class),
-                    "params": data.params.dataset,
-                },
-                "loss": {
-                    "class": full_name(data.loss_class),
-                    "params": data.params.loss,
-                },
-                "seed": data.seed,
-                "training": {"params": data.params.training},
-            },
-        )
+        params = data.params.copy()
+        value = []
+        for key in ("model", "dataset", "loss"):
+            value.append(
+                (
+                    dumper.represent_str(key),
+                    dumper.represent_mapping(
+                        tag="!" + full_name(getattr(data, key + "_class")),
+                        mapping=params.pop(key, {}),
+                    ),
+                )
+            )
+
+        value.extend(dumper.represent_data(params).value)
+        return yaml.MappingNode(tag="!" + full_name(data.__class__), value=value)
 
     @staticmethod
     def from_yaml(loader: yaml.Loader, node):
         """Load a ClassificationTask from a YAML file (registerd as constructor)"""
-        data = loader.construct_mapping(node, deep=True)
-        model_class = locate(data["model"]["class"])
-        dataset_class = locate(data["dataset"]["class"])
-        loss_class = locate(data["loss"]["class"])
-        params = Params(
-            model=data["model"]["params"],
-            dataset=data["dataset"]["params"],
-            loss=data["loss"]["params"],
-        )
-        seed = data.get("seed", None)
+        if not isinstance(node, yaml.MappingNode):
+            raise yaml.constructor.ConstructorError(
+                None,
+                None,
+                f"expected a mapping node, but found {node.id}",
+                node.start_mark,
+            )
+        data = {"params": {}}
+        for key_node, value_node in node.value:
+            key = loader.construct_object(key_node, deep=True)
+            if key in ("model", "dataset", "loss"):
+                data[key] = locate(strip_exclamation(value_node.tag))
+                data["params"][key] = loader.construct_mapping(value_node, deep=True)
+            else:
+                data["params"][key] = loader.construct_object(value_node, deep=True)
+
         return ClassificationTask(
-            model_class=model_class,
-            dataset_class=dataset_class,
-            params=params,
-            loss_class=loss_class,
-            seed=seed,
+            model_class=data["model"],
+            dataset_class=data["dataset"],
+            params=data["params"],
+            loss_class=data["loss"],
         )
 
 
@@ -106,3 +114,5 @@ if __name__ == "__main__":
     # task = ClassificationTask(CNN3, MNIST, Params(training={"batch_size": 128}))
     with open("task.yaml", "r", encoding="utf-8") as file:
         task = yaml.load(file, Loader=yaml.FullLoader)
+    
+    yaml.dump(task, stdout)
